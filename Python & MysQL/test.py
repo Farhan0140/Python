d9f389048ae97163cbd0d32e687bfdac091645a1
___Farhan_Nadim______ <122822790+Farhan0140@users.noreply.github.com>
2025-04-01 22:35:22 +0600

    Refactor database connection variable and add print statement for connection confirmation

--- Python & MysQL/test.py
+++ Python & MysQL/test.py
@@ -1,10 +1,10 @@
 import mysql.connector
+# print(mysql.connector.__version__)
 
-mydb = mysql.connector.connect(
+mydb_connection = mysql.connector.connect(
     host="localhost",
     user="root",
     passwd="admin"
 ) 
+print(mydb_connection)
 
-# print(mysql.connector.__version__)
-print(mydb)
