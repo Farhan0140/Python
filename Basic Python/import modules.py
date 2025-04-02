7c99d29d9b8da3aca03c5bffcd66d5e0af9e88f6
___Farhan_Nadim______ <122822790+Farhan0140@users.noreply.github.com>
2025-04-02 15:51:21 +0600

    Add import_modules.py to demonstrate importing and using functions from Function.py

---
+++ Basic Python/import modules.py
@@ -0,0 +1,6 @@
+from Function import return_multiple
+# from Function import return_multiple as r_m
+# from Function import *   # * Everything
+
+x = return_multiple( 10, 20 )
+print(f'From Function.py {x}')  # Inside import modules.py : [12, 8, 20, 5.0, 0]
