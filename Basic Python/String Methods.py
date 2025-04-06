392ddbfd3620a11df7d3498ed18676942f700853
___Farhan_Nadim______ <122822790+Farhan0140@users.noreply.github.com>
2025-04-06 21:22:03 +0600

    Add String Methods.py to demonstrate various string operations and methods in Python

---
+++ Basic Python/String Methods.py
@@ -0,0 +1,60 @@
+
+s = 'Farhan'
+s1 = "Nadim"
+s2 = """
+     
+     Farhan
+     Nadim
+     Sourav
+
+     """
+
+s3 = 'Farhan\'s'  # Escape
+
+
+# ------
+print('py' in 'python')   #  True
+
+
+# ------
+print( "The sum of 100 + 12 is {0}".format(100 + 12) )  #   The sum of 100 + 12 is 112
+
+
+# ------
+s = "farhan"
+s = s.upper()
+print( s )  #   FARHAN
+
+
+# ------
+s = "fARHan"
+s = s.lower()
+print( s )  #   farhan
+
+
+# ------
+s = "fARHan"
+print( s.isupper() )    #   False
+
+
+# ------
+s = "farhan"
+print( s.islower() )    #   True
+
+
+# ------
+s = '7'
+print( s.isdigit() )    #   True
+
+
+# ------
+s = 'A'
+print( s.isalpha() )    #   True
+
+
+# ------
+# ------
+# ------
+# ------
+# ------
+# ------
