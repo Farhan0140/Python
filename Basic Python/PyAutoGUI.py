6f83b8009c93887e2ba737f6ad3924b53966c8e3
___Farhan_Nadim______ <122822790+Farhan0140@users.noreply.github.com>
2025-04-08 18:33:25 +0600

    Update PyAutoGUI.py to correct the written character and add installation instruction

--- Basic Python/PyAutoGUI.py
+++ Basic Python/PyAutoGUI.py
@@ -2,7 +2,7 @@
 #   https://pyautogui.readthedocs.io/en/latest/
 
 
-#   
+#   pip install PyAutoGUI
 
 import pyautogui
 import time
@@ -10,7 +10,7 @@ import time
 time.sleep(4)
 
 for i in range(1, 50):
-    pyautogui.write('@')
+    pyautogui.write('Hello World', interval=0.10)
     pyautogui.write('M')
     time.sleep(1)
     pyautogui.press('enter')
