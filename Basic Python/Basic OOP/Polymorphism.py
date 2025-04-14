5fd0419c18faf3c4c915ebf9df237a8c98b6d8ee
___Farhan_Nadim______ <122822790+Farhan0140@users.noreply.github.com>
2025-04-14 15:00:41 +0600

    Implement Shape classes with area methods for Circle, Square, Triangle, and Pizza

--- Basic Python/Basic OOP/Polymorphism.py
+++ Basic Python/Basic OOP/Polymorphism.py
@@ -1,5 +1,79 @@
+
+# Polymorphism = Greek word that means to "have many forms or faces"
+
 # poly --> many (multiple)
 # morph --> shape
+
+
+# TWO WAYS TO ACHIEVE POLYMORPHISM
+# 1. Inheritance = An object could be treated of the same type as a parent class
+# 2. "Duck typing" = Object must have necessary attributes/methods
+
+#__________________________________________________________________
+
+
+
+
+from abc import ABC, abstractmethod
+
+class Shape(ABC):
+
+    @abstractmethod
+    def area(self):
+        pass
+
+
+class Circle(Shape):
+    def __init__(self, radius):
+        self.radius = radius
+
+    def area(self):
+        return 3.14 * self.radius ** 2
+
+
+class Square(Shape):
+    def __init__(self, side):
+        self.side = side
+
+    def area(self):
+        return self.side ** 2
+
+
+class Triangle(Shape):
+    def __init__(self, base, height):
+        self.base = base
+        self.height = height
+
+    def area(self):
+        return self.base * self.height * 0.5
+
+
+
+class Pizza(Circle):
+    def __init__(self, topping, radius):
+        super().__init__(radius)
+        self.topping = topping
+
+
+
+shapes = [Circle(4), Square(5), Triangle(6, 7), Pizza("pepperoni", 15)]
+
+
+
+for shape in shapes:
+    print(f"{shape.area()} cm²")     #  cm ^ 2 ---> 'Alt' + 0178
+
+
+
+
+
+
+
+
+# _______________________________________________________________________
+
+
+
 
 
 class Animal:
@@ -86,8 +160,4 @@ beh beh beh
 
 
 
-
-
-
-
-# ------------------------------------------
+# --------------------------------------------
