4be4cd0789a78b61f409a00a0cecd6b7213494f5
___Farhan_Nadim______ <122822790+Farhan0140@users.noreply.github.com>
2025-04-23 01:24:36 +0600

    Remove unused imports and commented-out code in Admin class

--- Projects/Restaurant Management System/_3_Admin.py
+++ Projects/Restaurant Management System/_3_Admin.py
@@ -1,9 +1,5 @@
 
 import _1_User as usr
-import _2_Employee as emp
-import _4_Restaurant as rest
-import _6_Food_Item as fd_itm
-import _7_Customer as cstmr
 
 
 class Admin( usr.User ):
@@ -35,63 +31,4 @@ class Admin( usr.User ):
 
     def show_menu(self, restaurant):
         restaurant.menu.show_menu()
-
-
-
-
-
-
-# _1_rest = rest.Restaurant('Suganda')
-# _1st_admin = Admin('Farhan', 23, '123123', 'asdasd@asd.com', 'dhaka', 'CEO')
-
-
-
-
-
-
-
-
-# _1_f = fd_itm.Food_Item('Pizza', 1200, 10)
-# _2_f = fd_itm.Food_Item('Burgir', 600, 5)
-
-
-# _1st_admin.add_item(_1_rest, _1_f)
-# _1st_admin.add_item(_1_rest, _2_f)
-
-# # _1st_admin.remove_item(_1_rest, 'Burgir')
-
-# # _1st_admin.show_menu(_1_rest)
-
-# _1_cstmr = cstmr.Customer('Mitu', '0144545', 'mitu@gmail.com', 'Bosti')
-# _1_cstmr.add_to_cart(_1_rest, 'pizza', 3)
-# _1_cstmr.add_to_cart(_1_rest, 'burgir', 3)
-
-# _1_cstmr.remove_from_cart(_1_rest, 'burgir')
-# # _1_cstmr.remove_from_cart(_1_rest, 'pizza')
-
-# _1_cstmr.view_cart()
-
-# _1st_admin.show_menu(_1_rest)
-
-
-
-
-
-
-
-# emp_1 = emp.Employee('Celia', 28, '1958177', 'fohdepid@gosve.fm', 'Monaco', 16865, 'Chef')
-# emp_2 = emp.Employee('Susan', 21, '1943319', 'weowaka@tosuhih.tw', 'Falkland Islands', 49427, 'Cashier')
-
-
-
-
-# _1st_admin.add_employee(_1_rest, emp_1)
-# _1st_admin.add_employee(_1_rest, emp_2)
-
-# _1st_admin.remove_employee(_1_rest, 'celia')
-
-# _1st_admin.view_employees(_1_rest)
-
-
-
 
