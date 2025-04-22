273d28f02b4b01dc634538d26f3cb6ff342a0bdb
___Farhan_Nadim______ <122822790+Farhan0140@users.noreply.github.com>
2025-04-22 23:23:10 +0600

    Add Order class with methods for managing order items and calculating total price

---
+++ Projects/Restaurant Management System/_8_Order.py
@@ -0,0 +1,36 @@
+
+
+class Order:
+
+    def __init__(self):
+        self.items = {}
+
+
+    def find_item(self, item_name):
+        for item in self.items:
+            if item.item_name.lower() == item_name.lower():
+                return item
+            
+        return None
+    
+
+    def add_item(self, item):
+        if item in self.items:
+            self.items[item] += item.quantity
+        else:
+            self.items[item] = item.quantity
+
+
+    def remove_item(self, item):
+        if item in self.items:
+            del self.items[item]
+
+
+    def total_price(self):
+        return sum( item.price * quantity  for item, quantity in self.items.items() )
+    
+
+    def clear_orders(self):
+        self.items = {}
+
+
