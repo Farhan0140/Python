803769a917698776687bd0005b62c2d04d06f355
___Farhan_Nadim______ <122822790+Farhan0140@users.noreply.github.com>
2025-04-25 20:14:10 +0600

    d

--- Projects/Shop.py/_4_Item.py
+++
@@ -1,9 +0,0 @@
-
-
-class Item:
-    def __init__(self, name, price, quantity, type):
-        self.name = name
-        self.price = price
-        self.quantity = quantity
-        self.type = type
-
