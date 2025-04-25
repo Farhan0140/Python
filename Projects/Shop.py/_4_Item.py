09a46f42e36ef8b71a35fdb44a0ee8beca345f93
___Farhan_Nadim______ <122822790+Farhan0140@users.noreply.github.com>
2025-04-25 15:14:16 +0600

    Add Item class with attributes for name, price, quantity, and type

---
+++ Projects/Shop.py/_4_Item.py
@@ -0,0 +1,9 @@
+
+
+class Item:
+    def __init__(self, name, price, quantity, type):
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+        self.type = type
+
