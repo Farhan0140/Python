9cb3237513d9869f545c8e38ed4efe7ee1801716
___Farhan_Nadim______ <122822790+Farhan0140@users.noreply.github.com>
2025-05-01 11:49:08 +0600

    Add Customer class for managing user interactions, ticket booking, and wallet functionality

---
+++ Projects/Cinema Ticket Booking System/_6_Customer.py
@@ -0,0 +1,69 @@
+
+from _1_User import User
+from _7_Order import Orders, Ticket
+
+
+class Customer( User ):
+    def __init__(self, name, email, password):
+        self.wallet = 0.0
+        self.cart = Orders()
+        super().__init__(name, email, password)
+
+
+    def update_info(self, cinema_hall, user):
+        cinema_hall.update_info(user)
+
+
+    def view_movie_list(self, cinema_hall):
+        cinema_hall.print_all_movie_info()
+
+
+    def book_ticket(self, cinema_hall, movie_name, sit_no):
+        movie = cinema_hall.find_using_name(movie_name, 'm')
+        if movie:
+            if (movie.ticket_price * sit_no) <= self.wallet:
+                ticket = Ticket()
+                ticket.add_ticket(movie, sit_no)
+                self.cart.add_to_cart(ticket)
+            else:
+                print(f'--- Insufficient Balance!! ---')
+        else:
+            print('--- Movie Not Found ---')
+
+
+    def add_balance(self, amount):
+        if amount > 0:
+            self.wallet += amount
+            print('--- Cash-in Successfully ---')
+            print(f'--- Current Balance is: {self.wallet} ---')
+        else:
+            print('--- Invalid Amount ---')
+
+
+    def view_available_sits(self, cinema_hall, movie_name):
+        movie = cinema_hall.find_using_name(movie_name, 'm')
+        if movie:
+            movie.sits.view_sit_map()
+        else:
+            print('--- Movie Not Found ---')
+
+
+    def pay_bill(self):
+        self.wallet -= self.cart.pay_bill()
+        
+    
+    def add_coupon(self, cinema_hall, coupon_code):
+        self.cart.add_coupon(cinema_hall, coupon_code)
+
+
+    def view_cart(self):
+        self.cart.view_cart()
+
+
+    def view_past_orders(self):
+        self.cart.view_past_orders()
+
+
+    def check_balance(self):
+        return self.wallet
+
